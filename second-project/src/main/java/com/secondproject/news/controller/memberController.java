013c7323b2a5875364256639ec320d6d95a9b574
qordjs700 <qordjs700@gmail.com>
2023-11-22 17:31:24 +0900

    클래스 파일 정리

--- second-project/src/main/java/com/secondproject/news/controller/memberController.java
+++ second-project/src/main/java/com/secondproject/news/controller/memberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.secondproject.news.domain.Member;
 import com.secondproject.news.service.MemberService;
 
-@Controllerx
+@Controller
 public class memberController {
 
 	@Autowired
